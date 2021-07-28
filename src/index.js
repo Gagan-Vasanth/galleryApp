bad71b39aade0b730d58cc7a5a1c0d02ec2233e5
Gagan Vasanth <mynameisgagan20@gmail.com>
2021-07-28 17:52:49 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_kEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSENGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+    apiKey: "AIzaSyB7Ii8aPVZ1ryiD2DnWTYfDFcknBp4PGN0",
+    authDomain: "gallery-app-88687.firebaseapp.com",
+    databaseURL: "https://gallery-app-88687-default-rtdb.firebaseio.com",
+    projectId: "gallery-app-88687",
+    storageBucket: "gallery-app-88687.appspot.com",
+    messagingSenderId: "244283831074",
+    appId: "1:244283831074:web:73d73ff3b1b0cf8323ab7b"
 };
 
 firebase.initializeApp(firebaseConfig);
